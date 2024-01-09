6e0627d3cda6944ca98aaadc8da01a0f512df825
Antonio Andre <antonioandre1008@gmail.com>
2024-01-09 13:39:07 -0500

    style: criacao do Historico de Transacoes

---
+++ src/components/HistoricoDeTransacoes.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bg-white p-6 rounded-xl">
+        <h1>Histórico de transações</h1>
+        <div class="flex flex-col gap-y-6 mt-6">
+            <div
+                class="flex justify-between bg-[#ccb3fc]/50 py-2 px-4 rounded-md text-[#9E6EFE]"
+            >
+                <h4>Ontem</h4>
+                <strong>R$ -540,32</strong>
+            </div>
+            <div
+                class="flex justify-between bg-[#ccb3fc]/50 py-2 px-4 rounded-md text-[#9E6EFE]"
+            >
+                <h4>Quinta-Feira</h4>
+                <strong>R$ -273,90</strong>
+            </div>
+            <div
+                class="flex justify-between bg-[#ccb3fc]/50 py-2 px-4 rounded-md text-[#9E6EFE]"
+            >
+                <h4>Segunda-Feira</h4>
+                <strong>R$ -1.456,20</strong>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,13 +1,17 @@
 <template>
     <main class="py-12 px-20">
         <Header />
-        <GraficoDeReceitas class="w-1/2" />
+        <div class="flex gap-x-6">
+            <GraficoDeReceitas class="w-1/2" />
+            <HistoricoDeTransacoes class="w-1/2" />
+        </div>
     </main>
 </template>
 
 <script setup>
-import GraficoDeReceitas from "../components/GraficoDeReceitas/GraficoDeReceitas.vue";
 import Header from "../components/Header.vue";
+import GraficoDeReceitas from "../components/GraficoDeReceitas/GraficoDeReceitas.vue";
+import HistoricoDeTransacoes from "../components/HistoricoDeTransacoes.vue";
 </script>
 
 <style lang="scss" scoped></style>
