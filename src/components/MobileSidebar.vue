44fb133be8dbfe250878b7a04ec05ed67bfca378
Antonio Andre <antonioandre1008@gmail.com>
2024-01-10 09:33:58 -0500

    feat: aplicacao funcionalidade Sidebar

--- src/components/MobileSidebar.vue
+++ src/components/MobileSidebar.vue
@@ -1,7 +1,7 @@
 <template>
-    <aside class="flex justify-between px-3 py-3">
-        <img class="cursor-pointer" src="/hamburguer.png" alt="" />
-        <div class="md:hidden flex items-center gap-x-8">
+    <aside class="flex justify-between px-3 py-3 md:hidden">
+        <img @click="handleOpen" class="cursor-pointer" src="/hamburguer.png" alt="" />
+        <div class="flex items-center gap-x-8">
             <div class="flex gap-x-4">
                 <img src="/search.png" alt="" />
                 <img src="/bell.png" alt="" />
@@ -11,8 +11,68 @@
             </div>
         </div>
     </aside>
+    <div
+        @click="handleOpen"
+        v-if="isOpen === true"
+        class="bg-black/20 w-screen h-screen absolute"
+    ></div>
+    <div
+        v-if="isOpen === true"
+        class="absolute bg-white flex flex-col items-center justify-center w-4/5 h-full"
+    >
+        <img
+            @click="handleOpen"
+            class="cursor-pointer absolute top-0 right-0 p-6"
+            src="/hamburguer.png"
+            alt=""
+        />
+
+        <h1 class="text-[#272727] text-3xl font-semibold">
+            <span class="text-[#9E6EFE]">e</span>Front
+        </h1>
+        <ol class="flex flex-col gap-y-6">
+            <span>
+                <li class="text-[#9E6EFE] bg-[#e8e0f7]/30 rounded-md py-2 px-3 flex gap-x-4">
+                    <img src="/dashboard.png" alt="" />
+                    Dashboard
+                </li>
+            </span>
+            <span class="flex gap-x-2">
+                <img src="/wallet.png" alt="" />
+                <li>Carteira</li>
+            </span>
+            <span class="flex gap-x-2">
+                <img src="/transactions.png" alt="" />
+                <li>Transações</li>
+            </span>
+            <span class="flex gap-x-2">
+                <img src="/support.png" alt="" />
+                <li>Suporte</li>
+            </span>
+            <span class="flex gap-x-2">
+                <img src="/settings.png" alt="" />
+                <li>Ajustes</li>
+            </span>
+            <div class="border border-gray-500/20"></div>
+        </ol>
+        <span class="flex items-center gap-x-2">
+            <h2 class="text-[#F03D3D] bg-[#fcb4b4]/30 rounded-md py-2 px-3 flex gap-x-4">
+                <img src="/disconnect.png" alt="" />
+                Desconectar
+            </h2>
+        </span>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const isOpen = ref(false);
+
+function handleOpen() {
+    isOpen.value = !isOpen.value;
+    console.log(isOpen.value);
+}
+</script>
 
 <style lang="scss" scoped></style>
